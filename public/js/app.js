d24f9b3b3be69a3b1d13eb9e7ea85fcf72ab41ce
Jon Miller <millerjw1124@gmail.com>
2021-02-10 15:52:56 -0800

    update to deploy on heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,16 +11,14 @@ weatherForm.addEventListener('submit', (e) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
 
-  fetch(`http://localhost:3000/weather?address=${location}`).then(
-    (response) => {
-      response.json().then((data) => {
-        if (data.error) {
-          messageOne.textContent = data.error
-        } else {
-          messageOne.textContent = `Location: ${data.location}`
-          messageTwo.textContent = `Forecast: ${data.forecastData}`
-        }
-      })
-    }
-  )
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageOne.textContent = data.error
+      } else {
+        messageOne.textContent = `Location: ${data.location}`
+        messageTwo.textContent = `Forecast: ${data.forecastData}`
+      }
+    })
+  })
 })
